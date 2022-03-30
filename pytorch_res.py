e8d5656d6fafdd74dbdf198ae3d9054064fa7021
Li_Harden <68153902+s07362022@users.noreply.github.com>
2022-03-30 22:44:43 +0800

    Create pytorch_res.py

---
+++ pytorch_res.py
@@ -0,0 +1,380 @@
+import torch.nn as nn
+import torch.utils.model_zoo as model_zoo
+
+
+__all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
+           'resnet152']
+
+
+model_urls = {
+    'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
+    'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
+    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
+    'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
+    'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
+}
+
+
+def conv3x3(in_planes, out_planes, stride=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
+                               padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, planes * self.expansion, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class ResNet(nn.Module):
+
+    def __init__(self, block, layers, num_classes=1000):
+        self.inplanes = 64
+        super(ResNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.avgpool = nn.AvgPool2d(7, stride=1)
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+
+def resnet18(pretrained=False, **kwargs):
+    """Constructs a ResNet-18 model.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(BasicBlock, [2, 2, 2, 2], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet18']))
+    return model
+
+
+def resnet34(pretrained=False, **kwargs):
+    """Constructs a ResNet-34 model.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(BasicBlock, [3, 4, 6, 3], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet34']))
+    return model
+
+
+def resnet50(pretrained=False, **kwargs):
+    """Constructs a ResNet-50 model.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet50']))
+    return model
+
+
+def resnet101(pretrained=False, **kwargs):
+    """Constructs a ResNet-101 model.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(Bottleneck, [3, 4, 23, 3], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet101']))
+    return model
+
+
+def resnet152(pretrained=False, **kwargs):
+    """Constructs a ResNet-152 model.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(Bottleneck, [3, 8, 36, 3], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet152']))
+    return model
+
+import torch
+import torch.nn as nn
+from torchvision import datasets ,transforms
+from pathlib import Path
+from matplotlib import pyplot as plt
+import numpy as np
+import torch.nn.functional as F
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from torch.nn import Linear, ReLU, CrossEntropyLoss, Conv2d, MaxPool2d, Module
+from torch.optim import Adam
+import pandas as pd
+import os
+from os import listdir
+from tqdm.notebook import tqdm
+from PIL import Image
+PATH_train="D:\\workspace\\stanford_dogs_dataset\\train\\"
+TRAIN =Path(PATH_train)
+#Batch：每批丟入多少張圖片
+batch_size = 16
+#Learning Rate：學習率
+LR = 0.002
+transforms = transforms.Compose([transforms.Resize((224,224)), transforms.ToTensor()]) #224,224
+
+train_data = datasets.ImageFolder(TRAIN, transform=transforms)
+print(train_data.class_to_idx)
+#切分訓練集、驗證集
+train_size = int(0.8 * len(train_data))
+valid_size = len(train_data) - train_size
+train_data, valid_data = torch.utils.data.random_split(train_data, [train_size, valid_size])
+#Dataloader可以用Batch的方式訓練
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size,shuffle=True)
+valid_loader = torch.utils.data.DataLoader(valid_data, batch_size=batch_size,shuffle=True)
+#print(type(train_loader))
+
+EPOCH = 1               # train the training data n times, to save time, we just train 1 epoch
+BATCH_SIZE = 16
+#LR = 0.003              # learning rate
+DOWNLOAD_MNIST = False
+
+#model = models.densenet161(num_classes=2)
+model =  resnet18()
+cnn = model
+def train(model,n_epochs,train_loader,valid_loader,optimizer,criterion):#,scheduler
+    train_acc_his,valid_acc_his=[],[]
+    train_losses_his,valid_losses_his=[],[]
+    for epoch in range(1, n_epochs+1):
+        # keep track of training and validation loss
+        train_loss,valid_loss = 0.0,0.0
+        train_losses,valid_losses=[],[]
+        train_correct,val_correct,train_total,val_total=0,0,0,0
+        train_pred,train_target=torch.zeros(8,1),torch.zeros(8,1)
+        val_pred,val_target=torch.zeros(8,1),torch.zeros(8,1)
+        count=0
+        count2=0
+        print('running epoch: {}'.format(epoch))
+        ###################
+        # train the model #
+        ###################
+        model.train()
+        for data, target in tqdm(train_loader):
+            # move tensors to GPU if CUDA is available
+            #if train_on_gpu: 
+                #data, target = data.cuda(), target.cuda()
+            # forward pass: compute predicted outputs by passing inputs to the model
+            #data, target = data.cpu(), target.cpu()
+            output = model(data)
+            # clear the gradients of all optimized variables
+            optimizer.zero_grad()
+            # calculate the batch loss
+            loss = criterion(output, target)
+            #calculate accuracy
+            pred = output.data.max(dim = 1, keepdim = True)[1]
+            train_correct += np.sum(np.squeeze(pred.eq(target.data.view_as(pred))).cpu().numpy())#.cpu()
+            train_total += data.size(0)
+            # backward pass: compute gradient of the loss with respect to model parameters
+            loss.backward()
+            # perform a single optimization step (parameter update)
+            optimizer.step()
+            #lr_scheduler.step()
+            # update training loss
+            train_losses.append(loss.item()*data.size(0))
+            
+            if count==0:
+                train_pred=pred
+                train_target=target.data.view_as(pred)
+                count=count+1
+            else:
+                train_pred=torch.cat((train_pred,pred), 0)
+                train_target=torch.cat((train_target,target.data.view_as(pred)), 0)
+        train_pred=train_pred.view(-1).cpu().numpy().tolist()#.cpu()
+        train_target=train_target.view(-1).cpu().numpy().tolist()#.cpu()
+######################    
+        # validate the model #
+        ######################
+        model.eval()
+        for data, target in tqdm(valid_loader):
+            # move tensors to GPU if CUDA is available
+            #if train_on_gpu:
+                #data, target = data.cuda(), target.cuda()
+            # forward pass: compute predicted outputs by passing inputs to the model
+            #data, target = data.cuda(), target.cuda()
+            output = model(data)
+            # calculate the batch loss
+            loss =criterion(output, target)
+            #calculate accuracy
+            pred = output.data.max(dim = 1, keepdim = True)[1]
+            val_correct += np.sum(np.squeeze(pred.eq(target.data.view_as(pred))).cpu().numpy())#.cpu()
+            val_total += data.size(0)
+            valid_losses.append(loss.item()*data.size(0))
+            if count2==0:
+                val_pred=pred
+                val_target=target.data.view_as(pred)
+                count2=count+1
+            else:
+                val_pred=torch.cat((val_pred,pred), 0)
+                val_target=torch.cat((val_target,target.data.view_as(pred)), 0)
+        val_pred=val_pred.view(-1).cpu().numpy().tolist()#.cpu()
+        val_target=val_target.view(-1).cpu().numpy().tolist()#.cpu()
+        
+        # calculate average losses
+        train_loss=np.average(train_losses)
+        valid_loss=np.average(valid_losses)
+        
+        # calculate average accuracy
+        train_acc=train_correct/train_total
+        valid_acc=val_correct/val_total
+        train_acc_his.append(train_acc)
+        valid_acc_his.append(valid_acc)
+        train_losses_his.append(train_loss)
+        valid_losses_his.append(valid_loss)
+# print training/validation statistics 
+        print('\tTraining Loss: {:.6f} \tValidation Loss: {:.6f}'.format(
+            train_loss, valid_loss))
+        print('\tTraining Accuracy: {:.6f} \tValidation Accuracy: {:.6f}'.format(
+            train_acc, valid_acc))
+    return train_acc_his,valid_acc_his,train_losses_his,valid_losses_his,model
+
+
+#model1=CNN_Model()
+model1 = cnn
+n_epochs = 10
+optimizer1 = torch.optim.Adam(model1.parameters() )#,lr=LR
+#########
+#lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer1, step_size=2)
+#scheduler_state = lr_scheduler.state_dict()
+
+##########
+
+
+#lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer1, step_size=2)#.ReduceLROnPlateau(optimizer1, 'max',verbose=1,patience=3)
+criterion = CrossEntropyLoss()
+#lr_scheduler.load_state_dict(scheduler_state)
+train_on_gpu = True
+train_acc_his,valid_acc_his,train_losses_his,valid_losses_his,model1=train(model1,n_epochs,train_loader,valid_loader,optimizer1,criterion)#,lr_scheduler
+#scheduler.step(val_loss)
+
+
+
+
+plt.figure(figsize=(15,10))
+plt.subplot(221)
+plt.plot(train_losses_his, 'bo', label = 'training loss')
+plt.plot(valid_losses_his, 'r', label = 'validation loss')
+plt.title("Simple CNN Loss")
+plt.legend(loc='upper left')
+plt.subplot(222)
+plt.plot(train_acc_his, 'bo', label = 'trainingaccuracy')
+plt.plot(valid_acc_his, 'r', label = 'validation accuracy')
+plt.title("Simple CNN Accuracy")
+plt.legend(loc='upper left')
+plt.show()
+torch.save(model1, "./fall_stand/back")
