6db7a19b39e715c5f5cc9ed37a1d30a40fc6d17a
Li_Harden <68153902+s07362022@users.noreply.github.com>
2022-07-27 14:20:46 +0800

    Create gpu_limit.py

---
+++ gpu_limit.py
@@ -0,0 +1,15 @@
+#tensorflow 2.2+可以加上這一段，限制GPU的使用量
+#主要是修改1024 -> 1GB
+
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+  # Restrict TensorFlow to only allocate 1GB of memory on the first GPU
+  try:
+    tf.config.set_logical_device_configuration(
+        gpus[0],
+        [tf.config.LogicalDeviceConfiguration(memory_limit=1024)])
+    logical_gpus = tf.config.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+  except RuntimeError as e:
+    # Virtual devices must be set before GPUs have been initialized
+    print(e)
